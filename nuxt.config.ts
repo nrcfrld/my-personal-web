f36a1ce761fda26224513487e6e65abc50ad3fec
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2022-12-02 16:16:33 +0700

    Update SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	ssr: false,
+	app: {
+		head: {
+			charset: "utf-16",
+			viewport: "width=500, initial-scale=1",
+			title:
+				"Enrico Feraldo Kalengkongang - UI/UX Design | Fullstack Web Developer",
+			meta: [
+				// <meta name="description" content="My amazing site">
+				{
+					name: "description",
+					content:
+						"Hello I'm Rico. I’m passionate in Product Digital Development, as a Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.",
+				},
+			],
+		},
+	},
 	css: ["~/assets/styles/main.scss"],
 	postcss: {
 		plugins: {
